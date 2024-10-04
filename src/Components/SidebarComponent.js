5bd4f94eafb52db04a4b9bb67881fc710c659561
Luis Arthur Cruz Sousa <luisarthurcruzsousa@MacBook-Air-de-Luis.local>
2024-10-04 11:33:53 -0300

    feat: logout button on sidebar

--- src/Components/SidebarComponent.js
+++ src/Components/SidebarComponent.js
@@ -15,7 +15,7 @@ const SidebarComponent = () => {
   return (
     <aside className="relative bg-[#151618] w-[227px] h-[95vh] mt-2 rounded-[12px] py-4 pl-4 ml-4 flex flex-col gap-y-5 text-white">
       {routes.map((route, index) => (
-        <div onClick={() => navigate(route.path)} key={index} className="flex h-ful items-center cursor-pointer">
+        <div onClick={() => navigate(route.path)} key={index} className="flex h-ful  cursor-pointer">
           <img className={`mr-2 ${location.pathname === route.path ? 'fill-[#2FFE82]' : ''}`} src={`/assets/images/icons/${route.icon}`} />
           <p
             className={`ml-2 text-[12px] font-bold ${location.pathname === route.path ? 'text-[#2FFE82]' : ''}`}
@@ -24,7 +24,7 @@ const SidebarComponent = () => {
           </p>
         </div>
       ))}
-      <button className="absolute bottom-[10px] flex py-1 flex px-2 justify-start w-[160px] items-center rounded-[10px] border-[#DB4831] bg-[#DB48313D] border-[1px]">
+      <button className="absolute bottom-[10px] flex py-1 flex px-2 justify-start w-[140px] items-center rounded-[10px] border-[#DB4831] bg-[#DB48313D] border-[1px]">
         <img className="mr-2" src={`/assets/images/icons/logoutIcon.svg`} />
         <p className=" ml-2 font-bold text-[#DB4831]">Sair</p>
       </button>
