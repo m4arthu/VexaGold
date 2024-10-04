3806ab8688a470c08255f82ba9c8c3073556a7a2
Luis Arthur Cruz Sousa <luisarthurcruzsousa@MacBook-Air-de-Luis.local>
2024-10-04 19:36:43 -0300

    feat: add select into rendimentos graphic

--- src/Components/Dashboard/Witgets/RendimentosChart.js
+++ src/Components/Dashboard/Witgets/RendimentosChart.js
@@ -10,7 +10,7 @@ const RendimentosChart = () => {
                 chart: {
                     id: "basic-bar",
                     type: "line",
-                    width:"100%",
+                    width: "100%",
                     sparkline: {
                         enabled: true
                     },
@@ -38,9 +38,14 @@ const RendimentosChart = () => {
     }, []);
 
     return (
-        <div className=" bg-[#151618] mt-4 mr-4 w-[60%] max-h-[300px]  rounded-[10px]">
-            <h1 className="text-white font-bold mt-4 ml-4">RENDIMENTOS</h1>
-            <div className=" w-full flex  p-2 items-end">
+        <div className=" bg-[#151618] mt-4 mr-4 w-[60%] max-h-[425px]  rounded-[10px]">
+            <div className="flex mt-4  w-full justify-between p-4 py-3">
+                <h1 className="text-white font-bold ml-4">RENDIMENTOS</h1>
+                <select class="appearance-none text-white font-bold  w-[160px] bg-[#1B985E] text-[14px] text-white  rounded-[10px] custom-select py-1 px-4">
+                    <option>ULTIMOS 7 DIAS</option>
+                </select>
+            </div>
+            <div className=" w-full h-[85%] flex  p-4 pt-0 mt-2 items-end">
                 <div className="text-white flex flex-col mr-3 mb-10">
                     {
                         [
@@ -63,7 +68,7 @@ const RendimentosChart = () => {
                             />
                         )}
                     </div>
-                    <div className="text-white flex gap-6">
+                    <div className="text-white mb-4 flex gap-6">
                         <p>1991</p> <p>1992</p> <p>1993</p> <p>1994</p> <p>1995</p> <p>1996</p> <p>1997</p> <p>1998</p> <p>1999</p>
                     </div>
                 </div>
