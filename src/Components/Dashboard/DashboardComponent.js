0ed28d3c18b20da5cade34c06f22119b92de0eee
Luis Arthur Cruz Sousa <luisarthurcruzsousa@MacBook-Air-de-Luis.local>
2024-10-04 09:53:08 -0300

    feat: dashboard responsivity

--- src/Components/Dashboard/DashboardComponent.js
+++ src/Components/Dashboard/DashboardComponent.js
@@ -1,6 +1,7 @@
 import AcertosWitget from "./Witgets/AcertosWitget"
 import LastOperationWitget from "./Witgets/LastOperationsWitget"
 import RendimentosChart from "./Witgets/RendimentosChart"
+import Top5ParidadesWitget from "./Witgets/Top5ParidadesWitget"
 import WalletDetailsWitget from "./Witgets/WalletDetailsWitget"
 
 const DashboardComponent = () => {
@@ -26,7 +27,7 @@ const DashboardComponent = () => {
             <div className="w-full justify-between flex">
                 <RendimentosChart/>
                 <AcertosWitget/>
-                <AcertosWitget/>
+                <Top5ParidadesWitget/>
             </div>
         </div>
     )
---
+++ src/Components/Dashboard/Witgets/Top5ParidadesWitget.js
@@ -0,0 +1,61 @@
+
+const Top5ParidadesWitget = () => {
+
+
+    return (
+        <div className="flex bg-[#151618] rounded-[10px] px-4 py-2 mt-4 mr-4  w-[35%] flex-col text-white ">
+            <h1 className="font-bold mt-2 mb-4">TOP 5 PARIDADES</h1>
+
+            <div className="w-full flex flex-col gap-4">
+                <div style={{ border: "1px solid #2E2F32" }} className="flex items-center py-4 pl-3 justify-center rounded-[8px] bg-[#2E2F323D] w-full h-[52px]">
+                    <div className="relative items-center w-full flex">
+                        <img width={18} src="assets/images/icons/brlJpy.svg" />
+                        <div className="ml-2 flex gap-3 items-center">
+                            <h1 className="text-[13px]">JPY/BRL</h1>
+                            <p className="text-[13px] text-[#2FFE82]">+27,6%</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ border: "1px solid #2E2F32" }} className="flex items-center py-4 pl-3 justify-center rounded-[8px] bg-[#2E2F323D] w-full h-[52px]">
+                    <div className="relative items-center w-full flex">
+                        <img width={18} src="assets/images/icons/brlJpy.svg" />
+                        <div className="ml-2 flex gap-3 items-center">
+                            <h1 className="text-[13px]">JPY/BRL</h1>
+                            <p className="text-[13px] text-[#2FFE82]">+27,6%</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ border: "1px solid #2E2F32" }} className="flex items-center py-4 pl-3 justify-center rounded-[8px] bg-[#2E2F323D] w-full h-[52px]">
+                    <div className="relative items-center w-full flex">
+                        <img width={18} src="assets/images/icons/brlJpy.svg" />
+                        <div className="ml-2 flex gap-3 items-center">
+                            <h1 className="text-[13px]">JPY/BRL</h1>
+                            <p className="text-[13px] text-[#2FFE82]">+27,6%</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ border: "1px solid #2E2F32" }} className="flex items-center py-4 pl-3 justify-center rounded-[8px] bg-[#2E2F323D] w-full h-[52px]">
+                    <div className="relative items-center w-full flex">
+                        <img width={18} src="assets/images/icons/brlJpy.svg" />
+                        <div className="ml-2 flex gap-3 items-center">
+                            <h1 className="text-[13px]">JPY/BRL</h1>
+                            <p className="text-[13px] text-[#2FFE82]">+27,6%</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ border: "1px solid #2E2F32" }} className="flex items-center py-4 pl-3 justify-center rounded-[8px] bg-[#2E2F323D] w-full h-[52px]">
+                    <div className="relative items-center w-full flex">
+                        <img width={18} src="assets/images/icons/brlJpy.svg" />
+                        <div className="ml-2 flex gap-3 items-center">
+                            <h1 className="text-[13px]">JPY/BRL</h1>
+                            <p className="text-[13px] text-[#2FFE82]">+27,6%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Top5ParidadesWitget;
